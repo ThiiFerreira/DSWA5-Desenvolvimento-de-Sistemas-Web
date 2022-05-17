3312acf2dc2ef575dbdbebf3bf726f7fcb2cb366
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-05-17 17:02:15 -0300

    Avaliação continua 10

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var app = require('./config/express')();
+const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.bodcm.mongodb.net:27017,cluster0-shard-00-01.bodcm.mongodb.net:27017,cluster0-shard-00-02.bodcm.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-fbwbp2-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
+});
